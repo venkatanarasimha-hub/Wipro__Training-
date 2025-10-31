fcfa672d5fee2e23f01ff79a389d7b882235e791
venkatanarasimha-hub <thanemnarasimha@gmail.com>
2025-10-31 15:21:22 +0530

    Update Question_4.java

--- Day2/Question_4.java
+++ Day2/Question_4.java
@@ -5,7 +5,7 @@ You have to make a string which consists of the same character. Find the minimum
 import java.util.Scanner;
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) { 
 
         //write your answer 
         Scanner sc = new Scanner(System.in);
