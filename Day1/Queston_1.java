adf70f85ac4818a6a7d94daf5e3b0e28e6a5b1a8
venkatanarasimha-hub <thanemnarasimha@gmail.com>
2025-10-31 15:15:47 +0530

    Arthimetic operations
    Uncommented input prompts for user clarity.

--- Day1/Queston_1.java
+++ Day1/Queston_1.java
@@ -9,7 +9,7 @@ public class ArithmeticOperations {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         // TODO: Write your code here
-       // System.out.println("Enter the first number: ");
+       // System.out.println("Enter the first number: "); 
         int num1 = scanner.nextInt();
         //System.out.println("Enter the second number: ");
         int num2 = scanner.nextInt();
