3f46c633a74da339913114bfe8ea0a623f7da352
venkatanarasimha-hub <thanemnarasimha@gmail.com>
2025-10-31 15:12:42 +0530

    Add logical operators example in Java
    Implement logical operations (AND, OR) on two boolean inputs.

---
+++ Day2/Logical Operators in Java.java
@@ -0,0 +1,18 @@
+/*Write a program that takes two boolean values as input and performs logical operations (AND, OR) on them using the logical operators& print the result.
+*/
+import java.util.Scanner;
+
+public class LogicalOperatorsExample {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        boolean input1 = scanner.nextBoolean();
+        boolean input2 = scanner.nextBoolean();
+        
+        // TODO: Write your code here
+        boolean andResult = input1 && input2;
+        boolean orResult = input1 || input2;
+        System.out.println(andResult);
+        System.out.println(orResult);
+        scanner.close();
+    }
+}
