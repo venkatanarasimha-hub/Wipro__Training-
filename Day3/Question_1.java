0e6d8e535e088d4de3df66b52a778497d8b50812
venkatanarasimha-hub <thanemnarasimha@gmail.com>
2025-10-31 15:24:28 +0530

    intial commit
    Implemented a 'Person' class with 'name' and 'age' attributes and created a program to input and display these values.

---
+++ Day3/Question_1.java
@@ -0,0 +1,25 @@
+/*Write a program that takes input from the user and creates an object of a class named 'Person'. The 'Person' class should have two member variables: 'name' and 'age'. The program should prompt the user to enter their 
+name and age, create a 'Person' object with the entered values, and then display the name and age of the person.*/
+
+import java.util.Scanner;
+
+public class Person {
+    // TODO: Define the 'Person' class with 'name' and 'age' member variables
+    String name;
+    int age;
+
+    public static void main(String[] args) {
+        // TODO: Write your code here
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        int age = sc.nextInt();
+        Person p = new Person();
+        p.name=name;
+        p.age=age;
+
+        System.out.println("Name: " + p.name);
+        System.out.println("Age: " + p.age);
+        sc.close();
+
+    }
+}
