58145effba11b22bc772b9c83a4cf204fb1bc3be
venkatanarasimha-hub <thanemnarasimha@gmail.com>
2025-10-31 15:06:30 +0530

    Add solution for minimum moves to equal characters
    Implement a solution to find the minimum moves to make all characters in a string equal.

---
+++ Day2/Question_4.java
@@ -0,0 +1,29 @@
+/*Equal character in string
+You are given a string s. In one move you can change any character to another character.
+
+You have to make a string which consists of the same character. Find the minimum move to do this task.*/
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+
+        //write your answer 
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+
+        int[] freq = new int[26];
+
+        for (int i = 0; i < s.length(); i++){
+            freq[s.charAt(i) - 'a']++;
+        }
+        int maxFreq=0;
+        for(int count : freq ){
+            maxFreq=Math.max(maxFreq,count);
+        }
+        int minMoves=s.length()-maxFreq;
+        System.out.println(minMoves);
+        sc.close();
+
+        
+    }
+}
